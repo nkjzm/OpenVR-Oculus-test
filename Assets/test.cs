272dffb4c81c5752631653ecec07e7997188f5a4
Nakaji Kohki <boomerang.cube@gmail.com>
2020-06-28 20:32:38 +0900

    Add files

---
+++ Assets/test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class test : MonoBehaviour
+{
+	private void Update()
+	{
+		// コントローラーのキーマッピングを調べるためのコード
+		for (var keyCode = KeyCode.JoystickButton0; keyCode <= KeyCode.JoystickButton19; ++keyCode)
+		{
+			if (Input.GetKeyDown(keyCode))
+			{
+				Debug.Log($"{keyCode}が反応しました。");
+			}
+		}
+	}
+}
